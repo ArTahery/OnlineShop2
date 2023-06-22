f2bf33f71415829602aa25a4d2aa28c07f60658e
unknown <alireza.ta1382@gmail.com>
2023-06-22 16:36:35 +0330

    version 1 of project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import DefaultLayout from './layouts/DefaultLayout.vue'
+import HomeViewVue from './views/HomeView.vue'
+</script>
+
+<template>
+<v-app id="app">
+<DefaultLayout></DefaultLayout>
+<v-main>
+<RouterView/>
+</v-main>
+</v-app>
+  
+</template>
+
+<style scoped>
+
+</style>
